07b56b2add1879ac96ea7d13a34d1f15a9b1706d
faiq khan <shayankhan480@gmail.com>
2022-05-12 17:21:36 +0500

    Design transfer money screen.

--- lib/screens/acount/transfer_money.dart
+++ lib/screens/acount/transfer_money.dart
@@ -1,10 +1,78 @@
 import 'package:flutter/material.dart';
 
-class TransferMoney extends StatelessWidget {
-const TransferMoney({Key? key}) : super(key: key);
+import '../../utils/assets.dart';
+import '../../utils/colors.dart';
+import '../../utils/constants.dart';
+import '../../utils/screen_properties.dart';
+import '../../utils/widget_util.dart';
+import '../../widgets/custom_button.dart';
+import '../../widgets/custom_dropdown.dart';
+import '../../widgets/custom_text_field.dart';
+import '../../widgets/simple_default_layout.dart';
+import '../../widgets/title_text.dart';
 
-@override
-Widget build(BuildContext context) {
-return Container();
-}
+class TransferMoney extends StatelessWidget {
+  const TransferMoney({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDefaultScreenLayout(
+      child: body(),
+    );
+  }
+
+  Widget body() {
+    return SingleChildScrollView(
+      padding: UIStyleProperties.insetsVrt8Hzt20,
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Center(
+            child: ScreenTitle(text: 'Transfer Money To Family',),
+          ),
+
+          CustomButton(
+            verticalMargin: 30,
+            label: 'Select Member',
+            trailing: "assets/images/up-arrow.png",
+            padding: UIStyleProperties.insetsVrt20Hzt10,
+            radius: 10,
+            onTap: () {},
+          ),
+
+          WidgetUtils.spaceVrt25,
+          CustomButton(
+            verticalMargin: 30,
+            label: 'Purpose',
+            trailing: "assets/images/up-arrow.png",
+            padding: UIStyleProperties.insetsVrt20Hzt10,
+            radius: 10,
+            onTap: () {},
+          ),
+
+          WidgetUtils.spaceVrt25,
+
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: ScreenTitle(text: 'Comments',),
+          ),
+
+          CustomTextField(
+            // label: '',
+            hintText: 'Enter Your Comments',
+            controller: TextEditingController(),
+            lines: 5,
+          ),
+
+          CustomButton(
+            label: 'Done',
+            // alignment: Alignment.center,
+            verticalMargin: 15,
+            onTap: () => null,
+          ),
+        ],
+      ),
+    );
+  }
 }
